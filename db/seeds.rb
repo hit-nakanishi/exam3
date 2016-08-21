1bb6d05555061be8126ac55935a04e5cb03097d5
hit-nakanishi <naka24naka24@gmail.com>
2016-08-21 07:01:02 +0000

    exam3_0821_6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
-n = 3
 10.times do |n|
   name = Faker::Internet.name
-  email = #{変数}"test@test.test"
+  email = "example-#{n}@example.org"
   password = "password"
   User.create!(
                email: email,
                name: name,
-	       password: password,
-               password_confirmation: password,
+               password: password
+	       password_confirmation: password,
               )
 end
