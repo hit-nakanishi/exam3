03c52bcb33d7c78d16ceccc70f6249366094f0f6
hit-nakanishi <naka24naka24@gmail.com>
2016-08-23 17:54:07 +0000

    fixed mailto address

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,23 +1,21 @@
 class NoticeMailer < ApplicationMailer
   def sendmail_topic(topic)
     @topic = topic
-    mail to: 'naka24naka24@gmail.com',
-    subject: '【exam3】トピックが投稿されました'
+    mail to: "naka24naka24@gmail.com",
+	    subject: '【exam3】トピックが投稿されました'
   end
 
   # お問い合わせ投稿（お客様）
   def sendmail_contact(contact)
     @contact = contact
-
     mail to: @contact.email,
-         subject: '【exam3】お問い合わせありがとうございます'
+	    subject: '【exam3】お問い合わせありがとうございます'
   end
 
   # お問い合わせ投稿（admin）
   def sendmail_contact_admin(contact2)
-    @contact = contact2
-    
-    mail to: 'naka24naka24@gmail.com',
-	    subject: '【exam3】お問い合わせが届きました'
+	  @contact = contact2
+	  mail to: "naka24naka24@gmail.com",
+		  subject: '【exam3】お問い合わせが届きました'
   end
 end
